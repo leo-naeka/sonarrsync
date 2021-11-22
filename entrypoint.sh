ec2e8d7054b7967fcd19f0746e3831a19e1d049a
leo-naeka <leo@naeka.fr>
2021-11-22 02:30:02 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,25 +3,25 @@
 # Create /Config.txt based on the environment variables we were passed
 
 cat << EOF > /Config.txt
-[Radarr]
-url = $SOURCE_RADARR_URL
-key = $SOURCE_RADARR_KEY
-path = $SOURCE_RADARR_PATH
+[Sonarr]
+url = $SOURCE_SONARR_URL
+key = $SOURCE_SONARR_KEY
+path = $SOURCE_SONARR_PATH
 
-[Radarr-target]
-url = $TARGET_RADARR_URL
-key = $TARGET_RADARR_KEY
-path_from = $SOURCE_RADARR_PATH
-path_to = $TARGET_RADARR_PATH
-# Sync movies coming _from_ the source in this quality profile
-profile = $SOURCE_RADARR_PROFILE_NUM
-# When adding movise to the destination Radarr, use _this_ quality profile (may differ from source)
-target_profile = $TARGET_RADARR_PROFILE_NUM
+[Sonarr-target]
+url = $TARGET_SONARR_URL
+key = $TARGET_SONARR_KEY
+path_from = $SOURCE_SONARR_PATH
+path_to = $TARGET_SONARR_PATH
+# Sync series coming _from_ the source in this quality profile
+profile = $SOURCE_SONARR_PROFILE_NUM
+# When adding series to the destination Sonarr, use _this_ quality profile (may differ from source)
+target_profile = $TARGET_SONARR_PROFILE_NUM
 EOF
 
 # Now execute the sync script in a loop, waiting DELAY before running again
 while true
 do
-	python /RadarrSync.py 
+	python /SonarrSync.py 
 	sleep $DELAY
 done
