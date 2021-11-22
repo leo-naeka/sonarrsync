ddbcc9fffdd553bde4cf934a104788efb2a05f67
leo-naeka <leo@naeka.fr>
2021-11-22 02:27:59 +0100

    Update and rename RadarrSync.py to SonarrSync.py

--- SonarrSync.py
+++ SonarrSync.py
@@ -37,7 +37,7 @@ def ConfigSectionMap(section):
             dict1[option] = None
     return dict1
 
-logger.debug('RadarSync Version {}'.format(VER))
+logger.debug('SonarrSync Version {}'.format(VER))
 Config = configparser.ConfigParser()
 
 # Loads an alternate config file so that I can work on my servers without uploading config to github
@@ -48,18 +48,18 @@ else:
     settingsFilename = os.path.join(os.getcwd(), 'Config.txt')
 Config.read(settingsFilename)
 
-radarr_url = ConfigSectionMap("Radarr")['url']
-radarr_key = ConfigSectionMap("Radarr")['key']
-radarrSession = requests.Session()
-radarrSession.trust_env = False
-radarrMovies = radarrSession.get('{0}/api/movie?apikey={1}'.format(radarr_url, radarr_key))
-if radarrMovies.status_code != 200:
-    logger.error('Radarr server error - response {}'.format(radarrMovies.status_code))
+sonarr_url = ConfigSectionMap("Sonarr")['url']
+sonarr_key = ConfigSectionMap("Sonarr")['key']
+sonarrSession = requests.Session()
+sonarrSession.trust_env = False
+sonarrSeries = sonarrSession.get('{0}/api/series?apikey={1}'.format(sonarr_url, sonarr_key))
+if sonarrSeries.status_code != 200:
+    logger.error('Sonarr server error - response {}'.format(sonarrSeries.status_code))
     sys.exit(0)
 
 for server in Config.sections():
 
-    if server == 'Default' or server == "Radarr":
+    if server == 'Default' or server == "Sonarr":
         continue  # Default section handled previously as it always needed
 
     else:
@@ -70,68 +70,68 @@ for server in Config.sections():
         SyncServer_url = ConfigSectionMap(server)['url']
         SyncServer_key = ConfigSectionMap(server)['key']
         SyncServer_target_profile = ConfigSectionMap(server)['target_profile']
-        SyncServerMovies = session.get('{0}/api/movie?apikey={1}'.format(SyncServer_url, SyncServer_key))
-        if SyncServerMovies.status_code != 200:
-            logger.error('4K Radarr server error - response {}'.format(SyncServerMovies.status_code))
+        SyncServerSeries = session.get('{0}/api/series?apikey={1}'.format(SyncServer_url, SyncServer_key))
+        if SyncServerSeries.status_code != 200:
+            logger.error('4K Sonarr server error - response {}'.format(SyncServerSeries.status_code))
             sys.exit(0)
 
-    # build a list of movied IDs already in the sync server, this is used later to prevent readding a movie that already
+    # build a list of series IDs already in the sync server, this is used later to prevent reading a series that already
     # exists.
-    # TODO refactor variable names to make it clear this builds list of existing not list of movies to add
-    # TODO #11 add reconcilliation to remove movies that have been deleted from source server
-    movieIds_to_syncserver = []
-    for movie_to_sync in SyncServerMovies.json():
-        movieIds_to_syncserver.append(movie_to_sync['tmdbId'])
-        #logger.debug('found movie to be added')
+    # TODO refactor variable names to make it clear this builds list of existing not list of series to add
+    # TODO #11 add reconcilliation to remove series that have been deleted from source server
+    seriesIds_to_syncserver = []
+    for series_to_sync in SyncServerSeries.json():
+        seriesIds_to_syncserver.append(series_to_sync['tmdbId'])
+        #logger.debug('found series to be added')
 
-    newMovies = 0
+    newSeries = 0
     searchid = []
-    for movie in radarrMovies.json():
-        if movie['profileId'] == int(ConfigSectionMap(server)['profile']):
-            if movie['tmdbId'] not in movieIds_to_syncserver:
-                logging.debug('title: {0}'.format(movie['title']))
-                logging.debug('qualityProfileId: {0}'.format(movie['qualityProfileId']))
-                logging.debug('titleSlug: {0}'.format(movie['titleSlug']))
-                images = movie['images']
+    for series in sonarrSeries.json():
+        if series['profileId'] == int(ConfigSectionMap(server)['profile']):
+            if series['tmdbId'] not in seriesIds_to_syncserver:
+                logging.debug('title: {0}'.format(series['title']))
+                logging.debug('qualityProfileId: {0}'.format(series['qualityProfileId']))
+                logging.debug('titleSlug: {0}'.format(series['titleSlug']))
+                images = series['images']
                 for image in images:
-                    image['url'] = '{0}{1}'.format(radarr_url, image['url'])
+                    image['url'] = '{0}{1}'.format(sonarr_url, image['url'])
                     logging.debug(image['url'])
-                logging.debug('tmdbId: {0}'.format(movie['tmdbId']))
-                logging.debug('path: {0}'.format(movie['path']))
-                logging.debug('monitored: {0}'.format(movie['monitored']))
+                logging.debug('tmdbId: {0}'.format(series['tmdbId']))
+                logging.debug('path: {0}'.format(series['path']))
+                logging.debug('monitored: {0}'.format(series['monitored']))
 
                 # Update the path based on "path_from" and "path_to" passed to us in Config.txt
-                path = movie['path']
+                path = series['path']
                 path = path.replace(ConfigSectionMap(server)['path_from'], ConfigSectionMap(server)['path_to'])
 
-                payload = {'title': movie['title'],
-                           'qualityProfileId': movie['qualityProfileId'],
-                           'titleSlug': movie['titleSlug'],
-                           'tmdbId': movie['tmdbId'],
+                payload = {'title': series['title'],
+                           'qualityProfileId': series['qualityProfileId'],
+                           'titleSlug': series['titleSlug'],
+                           'tmdbId': series['tmdbId'],
                            'path': path,
-                           'monitored': movie['monitored'],
+                           'monitored': series['monitored'],
                            'images': images,
                            'profileId': SyncServer_target_profile, 
                            'minimumAvailability': 'released'
                            }
 
-                r = session.post('{0}/api/movie?apikey={1}'.format(SyncServer_url, SyncServer_key), data=json.dumps(payload))
+                r = session.post('{0}/api/series?apikey={1}'.format(SyncServer_url, SyncServer_key), data=json.dumps(payload))
                 if r.status_code in (200, 201):
                      searchid.append(int(r.json()['id']))
-                     logger.info('adding {0} to {1} server'.format(movie['title'], server))
+                     logger.info('adding {0} to {1} server'.format(series['title'], server))
                 else:
                      logger.debug('Failed with error: {0}'.format(r.json()))
             else:
-                logging.debug('{0} already in {1} library'.format(movie['title'], server))
+                logging.debug('{0} already in {1} library'.format(series['title'], server))
         else:
-            logging.debug('Skipping {0}, wanted profile: {1} found profile: {2}'.format(movie['title'],
-                                                                                        movie['profileId'],
+            logging.debug('Skipping {0}, wanted profile: {1} found profile: {2}'.format(series['title'],
+                                                                                        series['profileId'],
                                                                                         int(ConfigSectionMap(server)['profile'])
                                                                                         ))
 
 
 
     if len(searchid):
-        payload = {'name' : 'MoviesSearch', 'movieIds' : searchid}
+        payload = {'name' : 'SeriesSearch', 'seriesIds' : searchid}
         session.post('{0}/api/command?apikey={1}'.format(SyncServer_url, SyncServer_key), data=json.dumps(payload))
 
